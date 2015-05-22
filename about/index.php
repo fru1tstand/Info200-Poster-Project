8f98530804526df30461346bac0cbf03d8ee6a18
Kodlee Yin <kodlee@kodleeshare.net>
2015-05-22 11:44:33 -0700

    Added about page

--- about/index.php
+++ about/index.php
@@ -139,7 +139,56 @@ usort($places, function(Library $a, Library $b) {
 </nav>
 
 <div class="container">
+	<div class="page-header">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1>UW StudyFinder</h1>
+				<p class="lead">About</p>
+			</div>
+		</div>
+	</div>
 
+	<div class="row">
+		<div class="col-lg-12">
+			<h3>Why we are here</h3>
+			<p>
+				Students and staff at the University of Washington often run into the issue of being unable to find a quiet place to study.
+				Our goal is to bring together these people with quality study spaces.
+				Be it simply a quiet place, a room with classmates, or a hangout spot with friends, our focus is to save time by figuring out how busy places are, so you don't have to.
+			</p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-12">
+			<h3>Who is using it?</h3>
+			<p>
+				It could be that student at the back of the class that always seems to ace mid-terms.
+				Or it could be that group that always sits together in lecture.
+				In any situation, that could be YOU joining that group and breezing through assignments and tests.
+				Students, staff, and faculty at the University of Washington all have access to this invaluable resource, so start using it today!
+			</p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-12">
+			<h3>What we provide</h3>
+			<p>
+				Because students and staff have a hard time finding quality study spaces, our objective is to find those spaces for you.
+				We provide insight for a majority of places available for all undergraduates, in-department students, graduates, and staff.
+				Our Info Favicon Display provides insightful icons that help you choose the right spot for your needs.
+				Log in to personalize the app, and find friends and classmates that are studying as well.
+			</p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-12">
+			<p>There's nothing to lose! <a href="/">Get started now!</</p>
+		</div>
+
+	</div>
 </div>
 
 
